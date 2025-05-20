6a0b306138e932097c25d776e084888d400c5521
清勇人 <y.sei91932@gmail.com>
2025-05-20 10:54:49 +0900

    #2 Create a registration form on the Sign Up page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,3 +1,53 @@
+import React, {useState} from 'react';
+
 export default function SignupPage () {
-    return <h1>Let's sign up!</h1>;
+    const [email, setEmail] = useState('');
+    const [userName, setUserName] = useState('');
+
+    const handleSumbit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const response = await fetch('/api/signup',{
+            method: 'POST',
+            headers: {'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email,
+                user_name: userName,
+            }),
+        });
+
+        if (response.ok) {
+            alert('User registered successfully!');
+        } else {
+            alert('Registration failed!');
+        }
+    };
+
+    return (
+        <div className="">
+            <h1 className=''>Sign up</h1>
+            <form onSubmit={handleSumbit} className=''>
+                <div>
+                    <label className='' >User Name</label>
+                    <input 
+                        type="text"
+                        value={userName} 
+                        onChange={(e) => setUserName(e.target.value)}
+                        required
+                        className=''
+                        />
+                </div>
+                <div>
+                    <label className=''>Email</label>
+                    <input 
+                        type="emial" 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        className=''/>
+                </div>
+                <button type="submit" className=''>Register</button>
+            </form>
+        </div>
+    );
 }
